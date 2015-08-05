513c5410836876e86dc6e77d6bdbadd24550da62
David Eads <davideads@gmail.com>
2015-08-05 15:47:59 -0400

    generic summarizer

--- summarize.py
+++ summarize.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python
 
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 import dataset
-
 
 POSTGRES_URL = 'postgresql:///worldvalues'
 db = dataset.connect(POSTGRES_URL)
 
-def default_factory():
-    return defaultdict(int)
+def initialize_counts(question_id):
+    table = db['categories']
+    categories = table.find(question_id=question_id)
+    category_counts = OrderedDict()
 
-def summarize_v45():
+    for category in categories:
+        category_counts[category['value']] = 0
+
+    return category_counts
+
+def summarize(question_id):
     result= db.query("""
         select 
             countries.value as country, c.value as response
         from
-            survey_responses r 
-        join 
-            (select * from categories where question_id='v45') c on r.v45=c.code
+            survey_responses r
+        join
+            (select * from categories where question_id='{0}') c on r.{0}=c.code
         join
             (select * from categories where question_id='v2a') countries on r.v2a=countries.code    
         ;
-    """)
+    """.format(question_id))
 
-    counts= defaultdict(default_factory)
+    counts = OrderedDict()
+
     for row in result:
-        counts[row["country"]][row["response"]] +=1
+        if not row['country'] in counts.keys():
+            counts[row['country']] = initialize_counts(question_id)
+
+        counts[row["country"]][row["response"]] += 1
+
     print counts
-    
+
 
 if __name__ == '__main__':
-    summarize_v45()
+    summarize('v54')
